eaa637d82a7f833fb92b24bb195f3db61c7dadcc
tuanpv29 <71309210+tuanpv29@users.noreply.github.com>
2021-02-25 21:38:31 +0700

    Create W3school Food blog template

---
+++ app.js
@@ -0,0 +1,9 @@
+const mySidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  mySidebar.style.display = "block";
+}
+
+function closeSidebar() {
+  mySidebar.style.display = "none";
+}
